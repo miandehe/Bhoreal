65c39bc89eabb38718be1099561ea67ab50c28ac
mdeheras <mdeheras@gmail.com>
2014-12-08 14:10:33 +0100

    Remap matrix

--- Bhoreal_Slim_0_1/Bhoreal.cpp
+++ Bhoreal_Slim_0_1/Bhoreal.cpp
@@ -82,44 +82,44 @@ byte remapSlim[4][8][8] =
 byte remapMATRIX[4][8][8] = 
 {
   {             // mapping matrix for Slim
-    {0,8,16,24,       32,40,48,56}, 
-    {1,9,17,25,       33,41,49,57}, 
-    {2,10,18,26,      34,42,50,58}, 
-    {3,11,19,27,      35,43,51,59}, 
-    {4,12,20,28,      36,44,52,60}, 
-    {5,13,21,29,      37,45,53,61}, 
-    {6,14,22,30,      38,46,54,62}, 
-    {7,15,23,31,      39,47,55,63}
+    {0,1,2,3,              4,5,6,7}, 
+    {8,9,10,11,        12,13,14,15}, 
+    {16,17,18,19,      20,21,22,23}, 
+    {24,25,26,27,      28,29,30,31}, 
+    {32,33,34,35,      36,37,38,39}, 
+    {40,41,42,43,      44,45,46,47}, 
+    {48,49,50,51,      52,53,54,55}, 
+    {56,57,58,59,      60,61,62,63}
   },  
   {             // mapping matrix for Slim
-    {7,6,5,4,              3,2,1,0}, 
-    {15,14,13,12,        11,10,9,8}, 
-    {23,22,21,20,      19,18,17,16}, 
-    {31,30,29,28,      27,26,25,24},
-    {39,38,37,36,      35,34,33,32}, 
-    {47,46,45,44,      43,42,41,40},
-    {55,54,53,52,      51,50,49,48},
-    {63,62,61,60,      59,58,57,56}
+    {56,48,40,32,       24,16,8,0}, 
+    {57,49,41,33,       25,17,9,1}, 
+    {58,50,42,34,      26,18,10,2}, 
+    {59,51,43,35,      27,19,11,3},
+    {60,52,44,36,      28,20,12,4}, 
+    {61,53,45,37,      29,21,13,5},
+    {62,54,46,38,      30,22,14,6},
+    {63,55,47,39,      31,23,15,7}
   },
   {             // mapping matrix for Slim
-    {63,55,47,39,       31,23,15,7}, 
-    {62,54,46,38,       30,22,14,6}, 
-    {61,53,45,37,       29,21,13,5}, 
-    {60,52,44,36,       28,20,12,4}, 
-    {59,51,43,35,       27,19,11,3}, 
-    {58,50,42,34,       26,18,10,2}, 
-    {57,49,41,33,        25,17,9,1}, 
-    {56,48,40,32,        24,16,8,0} 
+    {63,62,61,60,       59,58,57,56}, 
+    {55,54,53,52,       51,50,49,48}, 
+    {47,46,45,44,       43,42,41,40}, 
+    {39,38,37,36,       35,34,33,32}, 
+    {31,30,29,28,       27,26,25,24}, 
+    {23,22,21,20,       19,18,17,16}, 
+    {15,14,13,12,         11,10,9,8}, 
+    {7,6,5,4,               3,2,1,0} 
   },
   {             // mapping matrix for Slim
-    {56,57,58,59,      60,61,62,63}, 
-    {48,49,50,51,      52,53,54,55}, 
-    {40,41,42,43,      44,45,46,47}, 
-    {32,32,34,35,      36,37,38,39}, 
-    {24,25,26,27,      28,29,30,31}, 
-    {16,17,18,19,      20,21,22,23}, 
-    {8,9,10,11,        12,13,14,15}, 
-    {0,1,2,3,              4,5,6,7} 
+    {7,15,23,31,      39,47,55,63}, 
+    {6,14,22,30,      38,46,54,62}, 
+    {5,13,21,29,      37,45,53,61}, 
+    {4,12,20,28,      36,44,52,60}, 
+    {3,11,19,27,      35,43,51,59}, 
+    {2,10,18,26,      34,42,50,58}, 
+    {1,9,17,25,       33,41,49,57}, 
+    {0,8,16,24,       32,40,48,56} 
   }
 };
 
@@ -140,10 +140,10 @@ byte Bhoreal::slaveRead(byte reg)
     while (Wire.available()) c = Wire.read(); // receive byte as a character
     return c;
   }
+  
 //////////////////////////////////////////////////////////////////////
 //////////////////////       BHOREAL BEGIN      //////////////////////
 //////////////////////////////////////////////////////////////////////
-
 
 void Bhoreal::begin()
 {
@@ -425,6 +425,7 @@ void Bhoreal::checkButtons(){
       #if (MODEL == SLIMPRO)
         case 1:
           midiRefresh();
+          checkADC();
           // Check the button states
           checkMatrix(MIDI);
           break;
@@ -577,6 +578,13 @@ void Bhoreal::refresh(){
             }
         #endif
         #if (MODEL == SLIM) || (MODEL == SLIMPRO)
+//              Serial.print("Recibido: ");
+//              Serial.print(e.m2);
+//              Serial.print(' ');
+//              Serial.print(e.m2%8);
+//              Serial.print(' ');
+//              Serial.println(e.m2>>3);
+              
             if((e.type == 0x09) && (e.m3))  // NoteON midi message with vel > 0
             {  
               uint32_t c = hue2rgb(e.m3);  // velocity is used to HUE color selection and HUE is converted to RGB uint32 
@@ -584,14 +592,18 @@ void Bhoreal::refresh(){
                 r = (uint8_t)(c >> 16),
                 g = (uint8_t)(c >>  8),
                 b = (uint8_t)c;
-        
-              setPixelColor(remapSlim[GIR][e.m2%8][e.m2>>3], r, g, b);
+                
+
+              
+              //setPixelColor(remapSlim[GIR][e.m2%8][e.m2>>3], r, g, b);
+              setPixelColor(remapSlim[GIR][e.m2>>3][e.m2%8], r, g, b);
               refresh_led++;
               time_led = millis();
             }
             else if( (e.type == 0x08) || ((e.type == 0x09) && !e.m3) ) // NoteOFF midi message
             {  
-              setPixelColor(remapSlim[GIR][e.m2%8][e.m2>>3], 0, 0, 0);
+              //setPixelColor(remapSlim[GIR][e.m2%8][e.m2>>3], 0, 0, 0);
+              setPixelColor(remapSlim[GIR][e.m2>>3][e.m2%8], 0, 0, 0);
               refresh_led++;
               time_led = millis();
             }  
@@ -682,6 +694,7 @@ void Bhoreal::checkADC(){
             else if (limitx>5) GIR=0;
             else if (limity<2) GIR=1;
             else if (limity>5) GIR=3;  
+            //Serial.println(GIR);
     #else
         // For all of the ADC's which are activated, check if the analog value has changed,
         // and send a message if it has.
@@ -1823,6 +1836,7 @@ void Bhoreal::selectMode() {
   unsigned long time_blink = millis();
   while (mode == 0)
     {
+       checkADC();
        checkMatrix(SELECTOR);
        for(int x = 0; x < modeMAX; ++x) setPixelColor(remapSlim[GIR][x>>3][x%8], 255, 0, 0);
        for(int x = modeMAX; x < NUM_LEDS; ++x) setPixelColor(remapSlim[GIR][x>>3][x%8], 0, 0, 0);
